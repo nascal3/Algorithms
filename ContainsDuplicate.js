70b2622456af88709440df34080eb9ce0911ee61
nascal3 <nascal3@gmail.com>
2023-01-06 18:44:26 +0300

    added comments to code

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,6 +1,8 @@
 var containsDuplicate = function(nums) {
+    // sort the array ascending or descending order
     const sortedArry = nums.sort()
 
+    // loop through the array and compare if the current value is equal to the next value in the array
     const ArrLength = sortedArry.length
     for (let i = 0; i<=ArrLength; i++) {
         if (sortedArry[i] === sortedArry[i+1]) return true
--- validAnagram.js
+++ validAnagram.js
@@ -2,11 +2,15 @@ var isAnagram = function(s, t) {
     const arrayS = s.split('')
     const arrayT = t.split('')
 
+    // check if the words length are the same
     if (arrayS.length !== arrayT.length) return false
 
+    // create hashmaps/ objects to store count of characters in a word
     const sHash = {}
     const tHash = {}
 
+    // loop through the characters in the string and create a hashmap key from the characters,
+    // if it exists add a value of one to exiting value, if not assign value on one to that key
     const arrayLength = arrayS.length
     for (let i = 0; i<arrayLength; i++ ) {
         let charS = arrayS[i]
@@ -16,6 +20,7 @@ var isAnagram = function(s, t) {
         tHash[charT] =  tHash[charT] + 1 || 1
     }
 
+    // loop through the hashmap keys to compare if each key values match in both hashmaps/objects
     for (let key in sHash) {
         if (sHash[key] !== tHash[key]) return false
     }

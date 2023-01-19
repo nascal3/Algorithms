a96405950be4b060a6573cc5cff712ea06d8f176
nascal3 <nascal3@gmail.com>
2023-01-19 11:25:53 +0300

    three sum comments

--- ThreeSum.js
+++ ThreeSum.js
@@ -14,16 +14,17 @@ var threeSum = function(nums) {
         // Stop duplicated from occurrence by checking next value is equal to current value,
         // otherwise go loop again (ignore logic following in loop below)
         if (i > 0 && nums[i] === nums[i - 1]) continue
-        // Set the other two pointers (one on the left and one on the right) of array
+        // Set the other two pointers (one on the start and one on the end) of array
         let j = i + 1
         let k = nums.length - 1
 
-        // Check if the values of the sum where the two pointers are equal to zero
         while (j < k) {
+            // Check if the values of the sum where the three pointers are equal to zero,
+            // if so, store them in the result array created
             let sum = nums[i] + nums[j] + nums[k]
             if (sum === 0) {
                 ans.push([nums[i], nums[j], nums[k]])
-                // make sure next value in pointer is not a duplicate, if it is then move to next value
+                // Make sure next value in pointer is not a duplicate, if it is then move to next value
                 while(nums[j] === nums[j+1]) j++
                 while(nums[k] === nums[k+1]) k--
                 j++
@@ -38,6 +39,7 @@ var threeSum = function(nums) {
         }
 
     }
+    // Return the result Array containing the result set array
     return ans
 }
 

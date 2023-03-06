26f436788eb34e7ad8ead02ad1d305329b8bc521
nascal3 <nascal3@gmail.com>
2023-03-06 10:27:48 +0300

    Reverse linked list

---
+++ ReverseLinkedList.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+var reverseList = function(head) {
+    // Set the linked list pointers
+    let cur = head
+    let prev = null
+    let next
+
+    // Loop through the list
+    while (cur !== null) {
+        // Set next point to current.next.
+         next = cur.next
+         // Set current.next point to previuos.
+         cur.next = prev
+         // Set previous to point to current.
+         prev = cur
+         // Set current to point to next.
+         cur = next 
+    }
+
+    // Return previous
+    return prev
+    // console.log(prev);
+}
+
+
+const list = [1,2,3,4,5]
+reverseList(list)
